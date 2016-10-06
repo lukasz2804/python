25d39e525fbb0427466d360d52c7c142ec239487
student <szawara.lukasz@gmail.com>
2016-10-06 19:10:29 +0200

    jiojijiojio

---
+++ script1.py
@@ -0,0 +1,15 @@
+l=[1,1,2,3,4]
+l1 = l + [5,6,7]
+print l1
+l1.reverse()
+print l1
+l1.append(6)
+print l1
+l1.insert(4,42)
+print l1
+l1.pop()
+print l1
+l1.remove(5)
+print l1
+del l1[1:3]
+print l1
