1fb374d1afe2cdc22124807df2306ccaa5e8cf2e
student <szawara.lukasz@gmail.com>
2016-10-13 18:03:25 +0200

    scr1 cw2

--- script1.py
+++ script1.py
@@ -1,15 +1,8 @@
-l=[1,1,2,3,4]
-l1 = l + [5,6,7]
-print l1
-l1.reverse()
-print l1
-l1.append(6)
-print l1
-l1.insert(4,42)
-print l1
-l1.pop()
-print l1
-l1.remove(5)
-print l1
-del l1[1:3]
-print l1
+'''
+sdadas
+'''
+#konwersja liczby na napis
+a= 4
+b='6'
+c = `a` + b
+print c
